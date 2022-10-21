daff9f815189cf9daefe1dd965806aa726e69bac
s1nah4ck <sinadevcore@gmail.com>
2022-10-21 15:27:58 -0700

    update code

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -6,6 +6,10 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+
+
+
+
 class LoginController extends Controller
 {
     public function login(Request $request)
